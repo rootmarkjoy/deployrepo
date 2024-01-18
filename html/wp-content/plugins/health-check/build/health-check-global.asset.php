153461c078b79d8a34c982cf967abca5e3feee00
Mark <45856526+rootmarkjoy@users.noreply.github.com>
2024-01-18 17:11:30 +0530

    Delete html directory

--- html/wp-content/plugins/health-check/build/health-check-global.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '950cce4a6a8a23c6400e');
--- html/wp-content/plugins/health-check/build/health-check.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '08c8fbc45ae19e9130e1');
